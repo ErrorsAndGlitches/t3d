6491b68466f66987a829c792638d5559b21918cf
Benjamyn Ward <benjamyn.ward@gmail.com>
2013-03-21 19:55:29 -0400

    Remove makefile in favor of using premake Add initial premake script

---
+++ code/premake4.lua
@@ -0,0 +1,44 @@
+--
+-- Premake4 script to generate the 3D Tetris Game, T3D
+--
+
+-- build doxygen
+newaction {
+	trigger = "dox",
+	description = "Creates doxygen html and latex documentation files",
+	execute = function ()
+		os.execute("doxygen Doxyfile")
+	end
+}
+
+-- clean doxygen
+newaction {
+	trigger = "doxclean",
+	description = "Clean doxygen files",
+	execute = function ()
+		os.execute("rm -r html latex")
+	end
+}
+
+buildDir = "build"
+
+solution "T3D"
+	configurations { "Release", "Debug", "Benjamyn" }
+	targetname "t3d"
+	buildoptions { "-std=c++11", "-pipe" }
+
+project "T3D"
+	kind "WindowedApp"
+	language "C++"
+	includedirs { "include" }
+	files { "**.cpp", "**.h" } -- ** means recurse into directories
+	links { "glut", "GLU", "GL", "SDL", "SDL_image" }
+	location (buildDir)
+	objdir (buildDir)
+
+	configuration "Benjamyn"
+		buildoptions { "-O2", "-march=corei7" }
+
+	configuration "Debug"
+		flags { "ExtraWarnings" , "Symbols" }
+		
