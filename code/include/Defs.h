d3fe062c81972c0289a14386e98be4de06be787b
Benjamyn Ward <benjamyn.ward@gmail.com>
2013-04-22 20:54:26 -0400

    Add define's used in Jukebox.cpp to set up audio settings

---
+++ code/include/Defs.h
@@ -0,0 +1,15 @@
+#ifndef DEFS_H
+#define DEFS_H
+/**
+ * @file
+ * @brief This file provides a set of defines and macros
+ */
+
+#define MUSIC_FILENAME "data/beginning.mp3"
+
+#define AUDIO_RATE 22050
+#define AUDIO_FORMAT AUDIO_S16
+#define AUDIO_CHANNELS 2
+#define AUDIO_BUFFER 131072
+
+#endif
