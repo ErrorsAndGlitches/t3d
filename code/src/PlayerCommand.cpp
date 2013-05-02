9e906d6b66ffd5cea6767f248425f4997a281ff2
Zach Palchick <palchicz@terpmail.umd.edu>
2013-05-01 23:58:20 -0400

    Hooked in subarena processing -- maybe

--- code/src/PlayerCommand.cpp
+++ code/src/PlayerCommand.cpp
@@ -13,27 +13,35 @@ void PlayerCommand::execute(Subarena<> subarena)
 	switch (action)
 	{
 	case PlayerCommand::ROTATE_X:
+		subarena.rotateSuperBlock(SimpleRotation::RotationType::XCW);
 		std::cout << i++ << ": Map 0" << std::endl;
 		break;
 	case PlayerCommand::ROTATE_Y:
+		subarena.rotateSuperBlock(SimpleRotation::RotationType::YCW);
 		std::cout << i++ << ": Map 1" << std::endl;
 		break;
 	case PlayerCommand::ROTATE_Z:
+		subarena.rotateSuperBlock(SimpleRotation::RotationType::ZCW);
 		std::cout << i++ << ": Map 2" << std::endl;
 		break;
 	case PlayerCommand::MOVE_X_POS:
+		subarena.moveSuperBlockRelative(Vector(1, 0, 0));
 		std::cout << i++ << ": Map 3" << std::endl;
 		break;
 	case PlayerCommand::MOVE_X_NEG:
+		subarena.moveSuperBlockRelative(Vector(-1, 0, 0));
 		std::cout << i++ << ": Map 4" << std::endl;
 		break;
 	case PlayerCommand::MOVE_Y_POS:
+		subarena.moveSuperBlockRelative(Vector(0, 1, 0));
 		std::cout << i++ << ": Map 5" << std::endl;
 		break;
 	case PlayerCommand::MOVE_Y_NEG:
+		subarena.moveSuperBlockRelative(Vector(0, -1, 0));
 		std::cout << i++ << ": Map 6" << std::endl;
 		break;
 	case PlayerCommand::DROP_BLOCK:
+		subarena.newSuperBlock();
 		std::cout << i++ << ": Map 7" << std::endl;
 		break;
 	default:
