c058265d50ec9d8975e323143490ebeda753b75c
Benjamyn Ward <benjamyn.ward@gmail.com>
2013-05-14 04:44:40 -0400

    bug: in sending and receiving messages, the message size was not being converted to and from host/network format correctly

--- code/src/MessageCommunicator.cpp
+++ code/src/MessageCommunicator.cpp
@@ -3,26 +3,29 @@
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <cstdlib>
+#include <sys/select.h>
+#include <unistd.h>
 
 using namespace std;
 
 MessageCommunicator::MessageCommunicator() 
-{
-
-}
+{ }
 
 int MessageCommunicator::sendMessage(int socket, const void *msg, size_t msgSize)
 {
-	msgSize = ntohs(msgSize); // convert to network format
+	msgSize = htons(msgSize); // convert to network format
 
 	// send the size of the message
 	if (send(socket, &msgSize, sizeof(uint16_t), 0) < 0) {
 		cout << "NETWORK: message size send failed" << endl;
+		exit(-1);
 	}
 
 	// send the message
 	if (send(socket, msg, msgSize, 0) < 0) {
 		cout << "NETWORK: message send failed" << endl;
+		exit(-1);
 	}
 
 	return msgSize;
@@ -33,11 +36,14 @@ int MessageCommunicator::recvMessage(int socket, void *msg, size_t msgSize)
 	// receive the size of the message
 	if (recv(socket, &msgSize, sizeof(uint16_t), 0) < 0) {
 		cout << "NETWORK: message size receive failed" << endl;
+		exit(-1);
 	}
+	msgSize = ntohs(msgSize);
 
 	// receive the message
 	if (recv(socket, msg, msgSize, 0) < 0) {
 		cout << "NETWORK: message receive failed" << endl;
+		exit(-1);
 	}
 
 	return msgSize;
